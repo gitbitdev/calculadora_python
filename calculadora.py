77422bc33ed5d30928f6de99f31529e2ba872b4d
Alegt <alejandrogarciat2008@gmail.com>
2023-07-28 00:11:40 +0100

    Añadidas las funciones y algunas variables (AVISO: HAY DEBUGS PENDIENTES)

--- calculadora.py
+++ calculadora.py
@@ -5,28 +5,45 @@ secondNum = 0
 clicksInfo = list()
 firstNumDigits = 0
 secondNumDigits = 0
+seenNumbers = ""
+
+
+def buttonClicked(numOrSym, isANumber):
+    global clicksInfo
+    clicksInfo.append(numOrSym)
+    print(clicksInfo)
+
+def resetLabel():
+    global clicksInfo, firstNumDigits, secondNumDigits, seenNumbers
+    clicksInfo.clear()
+    firstNumDigits = 0
+    secondNumDigits = 0
+    seenNumbers = ""
+    label.config(text=seenNumbers)
 
 window = tk.Tk()
 window.title("Calculadora")
-label = tk.Label(window, text="0", padx=80, pady=20)
-button_0 = tk.Button(window, text="0", padx=20, pady=20)
-button_1 = tk.Button(window, text="1", padx=20, pady=20)
-button_2 = tk.Button(window, text="2", padx=20, pady=20)
-button_3 = tk.Button(window, text="3", padx=20, pady=20)
-button_4 = tk.Button(window, text="4", padx=20, pady=20)
-button_5 = tk.Button(window, text="5", padx=20, pady=20)
-button_6 = tk.Button(window, text="6", padx=20, pady=20)
-button_7 = tk.Button(window, text="7", padx=20, pady=20)
-button_8 = tk.Button(window, text="8", padx=20, pady=20)
-button_9 = tk.Button(window, text="9", padx=20, pady=20)
-button_division = tk.Button(window, text="÷", padx=20, pady=20)
-button_multiplicacion = tk.Button(window, text="×", padx=20, pady=20)
-button_resta = tk.Button(window, text="-", padx=20, pady=20)
-button_suma = tk.Button(window, text="+", padx=20, pady=20)
-button_decimal = tk.Button(window, text=",", padx=20, pady=20)
-button_igual = tk.Button(window, text="=", padx=20, pady=20)
+label = tk.Label(window, text="0", padx=60, pady=20)
+button_clear = tk.Button(window, text="C", padx=20, pady=20, command=resetLabel)
+button_0 = tk.Button(window, text="0", padx=20, pady=20, command=lambda:buttonClicked(0,True))
+button_1 = tk.Button(window, text="1", padx=20, pady=20, command=lambda:buttonClicked(1,True))
+button_2 = tk.Button(window, text="2", padx=20, pady=20, command=lambda:buttonClicked(2,True))
+button_3 = tk.Button(window, text="3", padx=20, pady=20, command=lambda:buttonClicked(3,True))
+button_4 = tk.Button(window, text="4", padx=20, pady=20, command=lambda:buttonClicked(4,True))
+button_5 = tk.Button(window, text="5", padx=20, pady=20, command=lambda:buttonClicked(5,True))
+button_6 = tk.Button(window, text="6", padx=20, pady=20, command=lambda:buttonClicked(6,True))
+button_7 = tk.Button(window, text="7", padx=20, pady=20, command=lambda:buttonClicked(7,True))
+button_8 = tk.Button(window, text="8", padx=20, pady=20, command=lambda:buttonClicked(8,True))
+button_9 = tk.Button(window, text="9", padx=20, pady=20, command=lambda:buttonClicked(9,True))
+button_division = tk.Button(window, text="÷", padx=20, pady=20, command=lambda:buttonClicked("/",False))
+button_multiplicacion = tk.Button(window, text="×", padx=20, pady=20, command=lambda:buttonClicked("*",False))
+button_resta = tk.Button(window, text="-", padx=20, pady=20, command=lambda:buttonClicked("-",False))
+button_suma = tk.Button(window, text="+", padx=20, pady=20, command=lambda:buttonClicked("+",False))
+button_decimal = tk.Button(window, text=",", padx=20, pady=20, command=lambda:buttonClicked(".",False))
+button_igual = tk.Button(window, text="=", padx=20, pady=20, command=lambda:buttonClicked("=",False))
 
-label.grid(row=0, columnspan=4, sticky="nsew")
+label.grid(row=0, columnspan=3, sticky="nsew")
+button_clear.grid(row=0, column=3, sticky="nsew")
 button_0.grid(row=4, column=0, sticky="nsew")
 button_1.grid(row=3, column=0, sticky="nsew")
 button_2.grid(row=3, column=1, sticky="nsew")
